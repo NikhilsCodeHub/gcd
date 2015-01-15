2e262285a114e92424da0bc01bebcac28a5f258c
Nikhil W <wnikhil@gmail.com>
2015-01-15 15:38:11 -0600

    update

---
+++ gettingDataViaOauth.R
@@ -0,0 +1,28 @@
+library(httr)
+
+# 1. Find OAuth settings for github:
+#    http://developer.github.com/v3/oauth/
+oauth_endpoints("github")
+
+# 2. Register an application at https://github.com/settings/applications;
+#    Use any URL you would like for the homepage URL (http://github.com is fine)
+#    and http://localhost:1410 as the callback url
+#
+#    Insert your client ID and secret below - if secret is omitted, it will
+#    look it up in the GITHUB_CONSUMER_SECRET environmental variable.
+myapp <- oauth_app("MyOauthRApp", "f3db086d8eec055495e0", secret="cdfe516195621af7cc3957c1384192d669f266d5
+")
+
+# 3. Get OAuth credentials
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+
+# 4. Use API
+gtoken <- config(token = github_token)
+req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+stop_for_status(req)
+content(req)
+
+# OR:
+req <- with_config(gtoken, GET("https://api.github.com/users/jtleek/repos"))
+stop_for_status(req)
+content(req)
